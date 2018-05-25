62b10dad429b1b7109f636511cb04402b0df8f80
Andrew Woodward <ebenwoodward@icloud.com>
2018-05-25 02:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function removeLastKitten () {
   return newKittens
 }
 
-  
+
   describe('removeFirstKitten()', function() {
     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
